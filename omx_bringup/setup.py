46cba9e8968629678ff89f2ba70840e6568a2e89
Polygon-13 <hyundiego@hanyang.ac.kr>
2026-07-16 01:49:39 +0900

    First Commit

---
+++ omx_bringup/setup.py
@@ -0,0 +1,36 @@
+from glob import glob
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'omx_bringup'
+
+setup(
+    name=package_name,
+    version='5.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config/omx_f'),
+            glob('config/omx_f/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name), ['open-manipulator-cdc.rules']),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Pyo',
+    maintainer_email='pyo@robotis.com',
+    description='OpenMANIPULATOR bringup ROS 2 package.',
+    license='Apache 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'joint_trajectory_executor = omx_bringup.joint_trajectory_executor:main',
+            'om_create_udev_rules = omx_bringup.om_create_udev_rules:main',
+        ],
+    },
+)
